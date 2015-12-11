aa7674e5cbda13a1cdbdbe4a822dc42bf7e36a6c
QCS18 <QCS18@PC-18.qcs.taiwan>
2015-12-11 11:55:42 +0800

    prepare disable right click

--- sites/all/themes/doming/html.tpl.php
+++ sites/all/themes/doming/html.tpl.php
@@ -53,7 +53,7 @@
   <?php print $scripts; ?>
   <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
-<body class="<?php print $classes; ?>" <?php print $attributes;?>>
+<body class="<?php print $classes; ?>" <?php print $attributes;?> oncontextmenu="return true">
   <div id="skip-link">
     <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
   </div>
