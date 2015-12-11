f1815919abb9a080ea881d97af87318a8043132b
QCS18 <QCS18@PC-18.qcs.taiwan>
2015-12-11 12:25:04 +0800

    temporary tracking analytics

--- sites/all/themes/doming/html.tpl.php
+++ sites/all/themes/doming/html.tpl.php
@@ -49,6 +49,17 @@
 <head profile="<?php print $grddl_profile; ?>">
   <?php print $head; ?>
   <title><?php print $head_title; ?></title>
+  <!-- Google Analytics -->
+  <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-XXXXX-Y', 'auto');
+      ga('send', 'pageview');
+  </script>
+  <!-- End Google Analytics -->
   <?php print $styles; ?>
   <?php print $scripts; ?>
   <meta name="viewport" content="width=device-width, initial-scale=1">
