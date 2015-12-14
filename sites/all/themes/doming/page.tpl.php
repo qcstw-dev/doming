40ec66832926cc41ff22e5f07a88031e76fe0d27
QCS18 <QCS18@PC-18.qcs.taiwan>
2015-12-14 17:48:56 +0800

    style menu dropdown, focus background colored

--- sites/all/themes/doming/page.tpl.php
+++ sites/all/themes/doming/page.tpl.php
@@ -77,7 +77,9 @@
 <script>
     $('ul.nav li.dropdown').hover(function() {
         $(this).find('.dropdown-menu').stop(true, true).show();
+        $(this).addClass('focus');
     }, function() {
         $(this).find('.dropdown-menu').stop(true, true).hide();
+        $(this).removeClass('focus');
     });
 </script>
