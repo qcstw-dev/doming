38547f4e3a55236d72c4c291ea7d1a7e52f8f9cd
QCS18 <QCS18@PC-18.qcs.taiwan>
2015-12-15 11:34:33 +0800

    style page "doming is durable" : bigger pictures

--- sites/all/themes/doming/node--doming_is_durable.tpl.php
+++ sites/all/themes/doming/node--doming_is_durable.tpl.php
@@ -24,26 +24,26 @@
             </div>
         </div>
         <div class="col-xs-12 padding-0">
-            <div class="col-xs-12 margin-bottom-10"><h4>Which makes it suitable for following industries:</h4></div>
-            <div class="col-xs-5 col-sm-2 border-none padding-0 margin-right-sm-20">
+            <h4>Which makes it suitable for following industries:</h4>
+            <div class="col-xs-5 col-sm-3">
                 <div class="thumbnail margin-bottom-10">
                     <img src="<?php print url("/sites/default/files/doming-is-durable/waterproof.jpg") ?>" alt="Doming is durable" title="Doming is durable" />
                 </div>
                 <p class="text-center">Title</p>
             </div>
-            <div class="col-xs-5 col-xs-offset-1 col-sm-offset-0 col-sm-2 border-none padding-0 margin-right-sm-20">
+            <div class="col-xs-5 col-sm-3">
                 <div class="thumbnail margin-bottom-10">
                     <img src="<?php print url("/sites/default/files/doming-is-durable/transportation.jpg") ?>" alt="Doming is durable" title="Doming is durable" />
                 </div>
                 <p class="text-center">Title</p>
             </div>
-            <div class="col-xs-5 col-sm-2 border-none padding-0 margin-right-sm-20">
+            <div class="col-xs-5 col-sm-3">
                 <div class="thumbnail margin-bottom-10">
                     <img src="<?php print url("/sites/default/files/doming-is-durable/sport.jpg") ?>" alt="Doming is durable" title="Doming is durable" />
                 </div>
                 <p class="text-center">Title</p>
             </div>
-            <div class="col-xs-5 col-xs-offset-1 col-sm-offset-0 col-sm-2 border-none padding-0 margin-right-sm-20">
+            <div class="col-xs-5 col-sm-3">
                 <div class="thumbnail margin-bottom-10">
                     <img src="<?php print url("/sites/default/files/doming-is-durable/promo.jpg") ?>" alt="Doming is durable" title="Doming is durable" />
                 </div>
@@ -56,51 +56,49 @@
             <img src="<?php print url("/sites/default/files/doming-is-durable/non-porous.jpg") ?>" alt="Doming is durable" title="Doming is durable" />
         </div>
         <div class="col-sm-8">
-            <div class="col-xs-12 text-center"><h3>Waterproof & temperature + UV resistant</h3></div>
-            <div class="col-xs-12">
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non felis risus. Suspendisse sit amet lectus facilisis.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non felis risus. Suspendisse sit amet lectus facilisis.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non felis risus. Suspendisse sit amet lectus facilisis.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non felis risus. Suspendisse sit amet lectus facilisis.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non felis risus. Suspendisse sit amet lectus facilisis.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non felis risus. Suspendisse sit amet lectus facilisis.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non felis risus. Suspendisse sit amet lectus facilisis.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non felis risus. Suspendisse sit amet lectus facilisis.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non felis risus. Suspendisse sit amet lectus facilisis.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non felis risus. Suspendisse sit amet lectus facilisis.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non felis risus. Suspendisse sit amet lectus facilisis.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non felis risus. Suspendisse sit amet lectus facilisis.
-                </p>
-            </div>
+            <h3 class="text-center">Waterproof & temperature + UV resistant</h3>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non felis risus. Suspendisse sit amet lectus facilisis.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non felis risus. Suspendisse sit amet lectus facilisis.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non felis risus. Suspendisse sit amet lectus facilisis.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non felis risus. Suspendisse sit amet lectus facilisis.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non felis risus. Suspendisse sit amet lectus facilisis.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non felis risus. Suspendisse sit amet lectus facilisis.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non felis risus. Suspendisse sit amet lectus facilisis.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non felis risus. Suspendisse sit amet lectus facilisis.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non felis risus. Suspendisse sit amet lectus facilisis.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non felis risus. Suspendisse sit amet lectus facilisis.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non felis risus. Suspendisse sit amet lectus facilisis.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non felis risus. Suspendisse sit amet lectus facilisis.
+            </p>
         </div>
         <div class="col-xs-12 padding-0">
-            <div class="col-xs-12 margin-bottom-10"><h4>Which makes it suitable for following industries:</h4></div>
-            <div class="col-xs-5 col-sm-2 border-none padding-0 margin-right-sm-20">
+            <h4>Which makes it suitable for following industries:</h4>
+            <div class="col-xs-5 col-sm-20-percent">
                 <div class="thumbnail margin-bottom-10">
                     <img src="<?php print url("/sites/default/files/doming-is-durable/3m.jpg") ?>" alt="Doming is durable" title="Doming is durable" />
                 </div>
                 <p class="text-center">Title</p>
             </div>
-            <div class="col-xs-5 col-xs-offset-1 col-sm-offset-0 col-sm-2 border-none padding-0 margin-right-sm-20">
+            <div class="col-xs-5 col-sm-20-percent">
                 <div class="thumbnail margin-bottom-10">
                     <img src="<?php print url("/sites/default/files/doming-is-durable/cat.jpg") ?>" alt="Doming is durable" title="Doming is durable" />
                 </div>
                 <p class="text-center">Title</p>
             </div>
-            <div class="col-xs-5 col-sm-2 border-none padding-0 margin-right-sm-20">
+            <div class="col-xs-5 col-sm-20-percent">
                 <div class="thumbnail margin-bottom-10">
                     <img src="<?php print url("/sites/default/files/doming-is-durable/hsbc.jpg") ?>" alt="Doming is durable" title="Doming is durable" />
                 </div>
                 <p class="text-center">Title</p>
             </div>
-            <div class="col-xs-5 col-xs-offset-1 col-sm-offset-0 col-sm-2 border-none padding-0 margin-right-sm-20">
+            <div class="col-xs-5 col-sm-20-percent">
                 <div class="thumbnail margin-bottom-10">
                     <img src="<?php print url("/sites/default/files/doming-is-durable/toys.jpg") ?>" alt="Doming is durable" title="Doming is durable" />
                 </div>
                 <p class="text-center">Title</p>
             </div>
-            <div class="col-xs-5 col-xs-offset-1 col-sm-offset-0 col-sm-2 border-none padding-0 margin-right-sm-20">
+            <div class="col-xs-5 col-sm-20-percent">
                 <div class="thumbnail margin-bottom-10">
                     <img src="<?php print url("/sites/default/files/doming-is-durable/rougie.jpg") ?>" alt="Doming is durable" title="Doming is durable" />
                 </div>
