e4c3b9b22988f9bfce22d9db7b930b0a68610a4b
QCS18 <QCS18@PC-18.qcs.taiwan>
2015-12-16 13:55:20 +0800

    page "import from china" administrable + doming.sql

--- sites/all/themes/doming/node--import-your-doming-from-china.tpl.php
+++ sites/all/themes/doming/node--import-your-doming-from-china.tpl.php
@@ -1,6 +1,6 @@
 <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
     <div class="col-xs-12 text-center">
-        <h2>Import your doming from China</h2>
+        <h2><?php print $field_import_main_title_1[0]['value'] ?></h2>
     </div>
     <div class="col-xs-12">
         <div class="col-sm-4 col-md-3">
@@ -9,30 +9,11 @@
             </div>
         </div>
         <div class="col-sm-8 col-md-9 margin-top-xs-10 text-justify">
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing 
-                elit. Donec non felis risus. Suspendisse sit amet lectus facilisis,
-                aliquam erat fermentum, dictum nisi. Ut euismod leo nulla, ut 
-                convallis leo dapibus cursus. Proin consectetur magna 
-                ac volutpat congue. Lorem ipsum dolor sit amet, consectetur adipiscing 
-                elit. Donec non felis risus. Suspendisse sit amet lectus facilisis,
-                aliquam erat fermentum, dictum nisi. Ut euismod leo nulla, ut 
-                convallis leo dapibus cursus. Proin consectetur magna 
-                ac volutpat congue.
-                Lorem ipsum dolor sit amet, consectetur adipiscing 
-                elit. Donec non felis risus. Suspendisse sit amet lectus facilisis,
-                aliquam erat fermentum, dictum nisi. Ut euismod leo nulla, ut 
-                convallis leo dapibus cursus. Proin consectetur magna 
-                ac volutpat congue. Lorem ipsum dolor sit amet, consectetur adipiscing 
-                elit. Donec non felis risus. Suspendisse sit amet lectus facilisis,
-                aliquam erat fermentum, dictum nisi. Ut euismod leo nulla, ut 
-                convallis leo dapibus cursus. Proin consectetur magna 
-                ac volutpat congue.
-            </p>
+            <p><?php print $field_import_text_1[0]['value'] ?></p>
         </div>
     </div>
     <div class="col-xs-12 text-center">
-        <h2>To buy significantly cheaper</h2>
+        <h2><?php print $field_import_main_title_2[0]['value'] ?></h2>
     </div>
     <div class="col-xs-12">
         <div class="col-sm-4 col-md-3">
@@ -41,30 +22,11 @@
             </div>
         </div>
         <div class="col-sm-8 col-md-9 margin-top-xs-10 text-justify">
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing 
-                elit. Donec non felis risus. Suspendisse sit amet lectus facilisis,
-                aliquam erat fermentum, dictum nisi. Ut euismod leo nulla, ut 
-                convallis leo dapibus cursus. Proin consectetur magna 
-                ac volutpat congue. Lorem ipsum dolor sit amet, consectetur adipiscing 
-                elit. Donec non felis risus. Suspendisse sit amet lectus facilisis,
-                aliquam erat fermentum, dictum nisi. Ut euismod leo nulla, ut 
-                convallis leo dapibus cursus. Proin consectetur magna 
-                ac volutpat congue.
-                Lorem ipsum dolor sit amet, consectetur adipiscing 
-                elit. Donec non felis risus. Suspendisse sit amet lectus facilisis,
-                aliquam erat fermentum, dictum nisi. Ut euismod leo nulla, ut 
-                convallis leo dapibus cursus. Proin consectetur magna 
-                ac volutpat congue. Lorem ipsum dolor sit amet, consectetur adipiscing 
-                elit. Donec non felis risus. Suspendisse sit amet lectus facilisis,
-                aliquam erat fermentum, dictum nisi. Ut euismod leo nulla, ut 
-                convallis leo dapibus cursus. Proin consectetur magna 
-                ac volutpat congue.
-            </p>
+            <p><?php print $field_import_text_2[0]['value'] ?></p>
         </div>
     </div>
     <div class="col-xs-12 text-center">
-        <h2>With quick delivery, low MOQ, 8 free prototypes !</h2>
+        <h2><?php print $field_import_main_title_1[0]['value'] ?></h2>
     </div>
     <div class="col-xs-12">
         <div class="col-sm-4 col-md-3">
@@ -73,26 +35,7 @@
             </div>
         </div>
         <div class="col-sm-8 col-md-9 margin-top-xs-10 text-justify">
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing 
-                elit. Donec non felis risus. Suspendisse sit amet lectus facilisis,
-                aliquam erat fermentum, dictum nisi. Ut euismod leo nulla, ut 
-                convallis leo dapibus cursus. Proin consectetur magna 
-                ac volutpat congue. Lorem ipsum dolor sit amet, consectetur adipiscing 
-                elit. Donec non felis risus. Suspendisse sit amet lectus facilisis,
-                aliquam erat fermentum, dictum nisi. Ut euismod leo nulla, ut 
-                convallis leo dapibus cursus. Proin consectetur magna 
-                ac volutpat congue.
-                Lorem ipsum dolor sit amet, consectetur adipiscing 
-                elit. Donec non felis risus. Suspendisse sit amet lectus facilisis,
-                aliquam erat fermentum, dictum nisi. Ut euismod leo nulla, ut 
-                convallis leo dapibus cursus. Proin consectetur magna 
-                ac volutpat congue. Lorem ipsum dolor sit amet, consectetur adipiscing 
-                elit. Donec non felis risus. Suspendisse sit amet lectus facilisis,
-                aliquam erat fermentum, dictum nisi. Ut euismod leo nulla, ut 
-                convallis leo dapibus cursus. Proin consectetur magna 
-                ac volutpat congue.
-            </p>
+            <p><?php print $field_import_text_3[0]['value'] ?></p>
         </div>
     </div>
 </div>
