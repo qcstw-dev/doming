18384122b1e31742bbd75e99213014027d0700c9
QCS18 <QCS18@PC-18.qcs.taiwan>
2015-12-16 14:01:27 +0800

    page "import from china" administrable images

--- sites/all/themes/doming/node--import-your-doming-from-china.tpl.php
+++ sites/all/themes/doming/node--import-your-doming-from-china.tpl.php
@@ -5,7 +5,7 @@
     <div class="col-xs-12">
         <div class="col-sm-4 col-md-3">
             <div class="thumbnail margin-auto">
-                <img src="<?php print url("/sites/default/files/import-from-china/import-from-china.jpg") ?>" alt="..." title="..." />
+                <img src="<?php print file_create_url($field_import_image_1[0]['uri']); ?>" alt="<?php print $field_import_image_1[0]['alt']; ?>" title="<?php print $field_import_image_1[0]['title']; ?>" />
             </div>
         </div>
         <div class="col-sm-8 col-md-9 margin-top-xs-10 text-justify">
@@ -18,7 +18,7 @@
     <div class="col-xs-12">
         <div class="col-sm-4 col-md-3">
             <div class="thumbnail margin-auto">
-                <img src="<?php print url("/sites/default/files/import-from-china/best-value.jpg") ?>" alt="..." title="..." />
+                <img src="<?php print file_create_url($field_import_image_2[0]['uri']); ?>" alt="<?php print $field_import_image_2[0]['alt']; ?>" title="<?php print $field_import_image_2[0]['title']; ?>" />
             </div>
         </div>
         <div class="col-sm-8 col-md-9 margin-top-xs-10 text-justify">
@@ -31,7 +31,7 @@
     <div class="col-xs-12">
         <div class="col-sm-4 col-md-3">
             <div class="thumbnail margin-auto">
-                <img src="<?php print url("/sites/default/files/import-from-china/rush.jpg") ?>" alt="..." title="..." />
+                <img src="<?php print file_create_url($field_import_image_3[0]['uri']); ?>" alt="<?php print $field_import_image_3[0]['alt']; ?>" title="<?php print $field_import_image_3[0]['title']; ?>" />
             </div>
         </div>
         <div class="col-sm-8 col-md-9 margin-top-xs-10 text-justify">
