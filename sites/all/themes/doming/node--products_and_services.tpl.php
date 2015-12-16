92e084e99b6557f74853c1906993452a902a364a
QCS18 <QCS18@PC-18.qcs.taiwan>
2015-12-16 12:32:57 +0800

    pages "durable" and "product and services" and "import from china"

--- sites/all/themes/doming/node--products_and_services.tpl.php
+++ sites/all/themes/doming/node--products_and_services.tpl.php
@@ -1,67 +1,52 @@
 <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>> 
-    <h2>Product features</h2>
-    <a id="a-block" href="<?php print url("node/".$field_link_1[0]['url']) ?>" title="<?php print $field_link_1[0]['title'] ?>">
-        <div class="col-xs-12 col-md-3 thumbnail border">
-            <img class="" src="<?php print file_create_url($field_image[0]['uri']); ?>" />
-            <h3 id="block-title" class="col-lg-12 text-center"><?php print $field_title_1[0]['value'] ?></h3>
-            <p class="col-lg-12 text-justify"><?php print $field_text_1[0]['value'] ?></p>
-        </div>
-    </a>
-    <div class="col-md-1 visible-md visible-lg"></div>
-    <a id="a-block" href="<?php print url("node/".$field_link_2[0]['url']) ?>" title="<?php print $field_link_2[0]['title'] ?>">
-        <div class="col-xs-12 col-md-3 thumbnail border">
-            <img class="" src="<?php print file_create_url($field_image_2[0]['uri']); ?>" />
-            <h3 id="block-title" class="col-lg-12 text-center"><?php print $field_title_2[0]['value'] ?></h3>
-            <p class="col-lg-12 text-justify"><?php print $field_text_2[0]['value'] ?></p>
-        </div>
-    </a>
-    <div class="col-md-1 visible-md visible-lg"></div>
-    <a id="a-block" href="<?php print url("node/".$field_link_3[0]['url']) ?>" title="<?php print $field_link_3[0]['title'] ?>">
-        <div class="col-xs-12 col-md-3 thumbnail border">
-            <img class="" src="<?php print file_create_url($field_image_3[0]['uri']); ?>" />
-            <h3 id="block-title" class="col-lg-12 text-center"><?php print $field_title_3[0]['value'] ?></h3>
-            <p class="col-lg-12 text-justify"><?php print $field_text_3[0]['value'] ?></p>
-        </div>
-    </a>
+    <h2><?php print $field_main_title_1[0]['value'] ?></h2>
+    <div class="col-xs-12">
+        <a id="a-block" href="<?php print url("node/".$field_link_1[0]['url']) ?>" title="<?php print $field_link_1[0]['title'] ?>">
+            <div class="col-xs-12 col-md-3 thumbnail border">
+                <img class="" src="<?php print file_create_url($field_image[0]['uri']); ?>" />
+                <h3 id="block-title" class="col-lg-12 text-center"><?php print $field_title_1[0]['value'] ?></h3>
+                <p class="col-lg-12 text-justify"><?php print $field_text_1[0]['value'] ?></p>
+            </div>
+        </a>
+        <div class="col-md-1 visible-md visible-lg"></div>
+        <a id="a-block" href="<?php print url("node/".$field_link_2[0]['url']) ?>" title="<?php print $field_link_2[0]['title'] ?>">
+            <div class="col-xs-12 col-md-3 thumbnail border">
+                <img class="" src="<?php print file_create_url($field_image_2[0]['uri']); ?>" />
+                <h3 id="block-title" class="col-lg-12 text-center"><?php print $field_title_2[0]['value'] ?></h3>
+                <p class="col-lg-12 text-justify"><?php print $field_text_2[0]['value'] ?></p>
+            </div>
+        </a>
+        <div class="col-md-1 visible-md visible-lg"></div>
+        <a id="a-block" href="<?php print url("node/".$field_link_3[0]['url']) ?>" title="<?php print $field_link_3[0]['title'] ?>">
+            <div class="col-xs-12 col-md-3 thumbnail border">
+                <img class="" src="<?php print file_create_url($field_image_3[0]['uri']); ?>" />
+                <h3 id="block-title" class="col-lg-12 text-center"><?php print $field_title_3[0]['value'] ?></h3>
+                <p class="col-lg-12 text-justify"><?php print $field_text_3[0]['value'] ?></p>
+            </div>
+        </a>
+    </div>
     <div class="clearfix"></div>
-    <h2>Service policy</h2>
-    <a id="a-block" href="<?php print url("node/6") ?>" title="Service policy">
+    <h2><?php print $field_main_title_2[0]['value'] ?></h2>
+    <a id="a-block" href="<?php print url("node/".$field_block_4_5_6_link[0]['url']) ?>" title="<?php print $field_block_4_5_6_link[0]['title'] ?>">
         <div class="col-xs-12">
             <div class="col-xs-12 col-md-3 thumbnail border">
-                <img class="" src="<?php print url("/sites/default/files/products-and-services/import-from-china.jpg") ?>" />
-                <h3 id="block-title" class="col-lg-12 text-center">Import from china</h3>
-                <p class="col-lg-12 text-justify">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing 
-                    elit. Donec non felis risus. Suspendisse sit amet lectus facilisis,
-                    aliquam erat fermentum, dictum nisi. Ut euismod leo nulla, ut 
-                    convallis leo dapibus cursus. Proin consectetur magna 
-                    ac volutpat congue.
-                </p>
+                <img class="" src="<?php print file_create_url($field_image_4[0]['uri']); ?>" />
+                <h3 id="block-title" class="col-lg-12 text-center"><?php print $field_title_4[0]['value'] ?></h3>
+                <p class="col-lg-12 text-justify"><?php print $field_text_4[0]['value'] ?></p>
             </div>
             <div class="col-md-1 arrow-right visible-md visible-lg"><span class="glyphicon glyphicon-arrow-right"></span></div>
             <div class="col-xs-12 arrow-down visible-sm visible-xs text-center"><span class="glyphicon glyphicon-arrow-down"></span></div>
             <div class="col-xs-12 col-md-3 thumbnail border">
-                <img class="" src="<?php print url("/sites/default/files/products-and-services/best-value.jpg") ?>" />
-                <h3 id="block-title" class="col-lg-12 text-center">To buy significantly cheaper</h3>
-                <p class="col-lg-12 text-justify">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing 
-                    elit. Donec non felis risus. Suspendisse sit amet lectus facilisis,
-                    aliquam erat fermentum, dictum nisi. Ut euismod leo nulla, ut 
-                    convallis leo dapibus cursus. Proin consectetur magna 
-                    ac volutpat congue.
-                </p>
+                <img class="" src="<?php print file_create_url($field_image_5[0]['uri']); ?>" />
+                <h3 id="block-title" class="col-lg-12 text-center"><?php print $field_title_5[0]['value'] ?></h3>
+                <p class="col-lg-12 text-justify"><?php print $field_text_5[0]['value'] ?></p>
             </div>
             <div class="col-md-1 arrow-right visible-md visible-lg"><span class="glyphicon glyphicon-arrow-right"></span></div>
             <div class="col-xs-12 arrow-down visible-sm visible-xs text-center"><span class="glyphicon glyphicon-arrow-down"></span></div>
             <div class="col-xs-12 col-md-3 thumbnail border">
-                <img class="" src="<?php print url("/sites/default/files/products-and-services/rush.jpg") ?>" />
-                <h3 id="block-title" class="col-lg-12 text-center">With quick delivery, low MOQ, 8 free prototypes</h3>
-                <p class="col-lg-12 text-justify">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing 
-                    elit. Donec non felis risus. Suspendisse sit amet lectus facilisis,
-                    aliquam erat fermentum, dictum nisi. Ut euismod leo nulla, ut 
-                    convallis leo dapibus cursus.
-                </p>
+                <img class="" src="<?php print file_create_url($field_image_6[0]['uri']); ?>" />
+                <h3 id="block-title" class="col-lg-12 text-center"><?php print $field_title_6[0]['value'] ?></h3>
+                <p class="col-lg-12 text-justify"><?php print $field_text_6[0]['value'] ?></p>
             </div>
         </div>
     </a>
