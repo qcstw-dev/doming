de0dfc02fd3ec261ee51f10662bc40af203de558
QCS18 <QCS18@PC-18.qcs.taiwan>
2016-01-04 15:07:46 +0800

    bloquage IP, mise en BDD des IP bloquées et non bloquées

--- sites/all/themes/doming/html.tpl.php
+++ sites/all/themes/doming/html.tpl.php
@@ -7,47 +7,51 @@ if (!isset($_SESSION['country']) || !$_SESSION['country']) {
         $ip = $_SERVER['REMOTE_ADDR'];
     }
     
-    
 //    test china
-//    $url = "http://freegeoip.net/json/113.100.99.221";
-    $url = "http://freegeoip.net/json/" . $ip;
+//    $url = "http://ip2c.org/113.100.99.221";
+    $url = "http://ip2c.org/" . $ip;
 
     set_time_limit(10);
 
     $data = file_get_contents($url);
+    $reply = explode(';',$data);
 
-    $obj = json_decode($data);
-
-    if (isset($obj->country_code) && $obj->country_code) {
-        $_SESSION['country'] = $obj->country_code;
+    if (isset($reply[1]) && $reply[1]) {
+        $_SESSION['country'] = $reply[1];
     } else {
         $_SESSION['country'] = '';
     }
+    if (in_array($_SESSION['country'], ['CN', 'KR', 'KP', 'TR', 'IN'])) {
+        $sqlQuery = "INSERT INTO ip_blocked (ip, country, date) VALUES ('".$ip."', '".$_SESSION['country']."', CURRENT_TIME)";
+    } else {
+        $sqlQuery = "INSERT INTO ip_unblocked (ip, country, date) VALUES ('".$ip."', '".$_SESSION['country']."', CURRENT_TIME)";
+    }
+    db_query($sqlQuery);
 }
 if (in_array($_SESSION['country'], ['CN', 'KR', 'KP', 'TR', 'IN'])) {
     echo 'This website is not available in your country';
     exit;
-}
-?>
+} ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN"
   "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" version="XHTML+RDFa 1.0" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?>>
 
 <head profile="<?php print $grddl_profile; ?>">
   <?php print $head; ?>
-  <title><?php print $head_title; ?></title>
-  <!-- Google Analytics -->
-  <script>
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  <title><?php print $head_title; ?></title><?php
+  if ($_SERVER["HTTP_HOST"] != 'localhost') { ?>
+      <!-- Google Analytics -->
+      <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-    ga('create', 'UA-5861473-6', 'auto');
-    ga('send', 'pageview');
-
-  </script>
-  <!-- End Google Analytics -->
+        ga('create', 'UA-5861473-6', 'auto');
+        ga('send', 'pageview');
+      </script>
+      <!-- End Google Analytics --><?php
+  } ?>
   <?php print $styles; ?>
   <?php print $scripts; ?>
   <meta property="og:image" content="http://doming-stickers.com/sites/default/files/home/doming.jpg" />
