03b46185d24cf753bb8a8fced8d158f9b87ced16
QCS18 <QCS18@PC-18.qcs.taiwan>
2016-02-02 16:08:11 +0800

    customizable page about us

--- sites/all/themes/doming/node--about_us.tpl.php
+++ sites/all/themes/doming/node--about_us.tpl.php
@@ -1,48 +1,19 @@
 <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
-    <h2 class="col-xs-12">About us</h2>
+    <h2 class="col-xs-12"><?php print $title ?></h2>
     <div class="col-xs-12 text-justify padding-0">
         <div class="col-xs-12 col-sm-5 col-md-4 thumbnail">
-            <img src="<?php print url("/sites/default/files/about-us/about-us-photo.jpg") ?>" alt="" title="" />
+            <img src="<?php print file_create_url($field_about_us_image[0]['uri']); ?>" alt="" title="" />
         </div>
-        <div class="col-sm-7 col-md-8">
-            <p>QCS is a Taiwanese manufacturer & exporter specialized in promotional product industry since 1991 with a factory in China since 1999 (more information about this <a href="https://www.qcsasia.com/our-factory" title="Our factory">here</a>) </p>
-
-            <p>To face China’s labor cost increase & shortage and to answer increasingly small MOQ & rush delivery request in the promotional product industry, we started to invest in doming logo process set-up in 2010. </p>
-
-            <p>Strong with our focus on service and quality, we decided not to improvise but to build a state of the art doming facility. <strong>For that purpose, we imported our domed badges as well as our raw material from UK & Italy and built a 300 m2 environment controlled production room and storage for curing and warehousing.</strong></p>
-
-            <p>Combining international high quality standard, low labor cost in China and 25+ years’ experience in logistic, QCS is proud to claim to be the most competitive doming label and label producer & exporter in China. </p>
-
-            <p>Our worldwide network of long term customers and partners is providing us a steady average volume, that contributes to guarantee optimum quality 3D doming production all year round. </p>
-
-            <p>Located in Zhaoqing City, 80 KM North West of Guangzhou, (Guangdong, China), you are welcome to visit our factory any time at your convenience.The canton fair, where we exhibit twice a year since 2004, is a great opportunity. </p>
-
-        </div>
+        <div class="col-sm-7 col-md-8"><?php print $field_about_us_text[0]['value'] ?></div>
     </div>
     <div class="col-xs-12 col-md-6 padding-left-0">
-        <h3>Taiwan head office :</h3>
-        <p>
-            QCS ASIA CO. ,LTD <span class="font-dfkai font-size-20">台灣妍品有限公司</span><br />
-            5F-8, 63 HEPING E. RD , SEC3 TAIPEI TAIWAN<br />
-            <span class="font-dfkai font-size-20">台北市和平東路3段63號5樓之8 (嘉樂大樓)</span><br />
-            <br />
-            TEL : (886-2) 27385787 FAX : (886-2)27385816
-        </p>
-        <div class="col-xs-12 margin-top-20 padding-0">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d903.8183351866223!2d121.54866132923432!3d25.024796214206898!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442aa321d20e10d%3A0xf43ccf1621215ddf!2sNo.+63%2C+Section+3%2C+Heping+E+Rd%2C+Da%E2%80%99an+District%2C+Taipei+City%2C+106!5e0!3m2!1sen!2stw!4v1452219366587" width="100%" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
-        </div>
+        <h3><?php print $field_title_address_taiwan[0]['value'] ?></h3>
+        <?php print $field_address_taiwan[0]['value'] ?>
+        <div class="col-xs-12 margin-top-20 padding-0"><?php print $field_map_localisation_taiwan[0]['value'] ?></div>
     </div>
     <div class="col-xs-12 col-md-6 padding-right-0">
-        <h3>China Factory:</h3>
-        <p>
-            QCS Gift Factory <span class="font-dfkai font-size-20">高要市廣星禮品有限公司</span><br />
-            D2 DISTRICT, JINDU, GAOYAO CITY, GUANGDONG, CHINA<br />
-            <span class="font-dfkai font-size-20">中國廣東省肇慶高要市金渡鎮D2小區</span> ZIP: 526108<br />
-            <br />
-            TEL :(86.758) 8512115 FAX : (86.758) 8512145
-        </p>
-        <div class="col-xs-12 margin-top-20 padding-0">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3671.455377450762!2d112.52200431537644!3d23.043760921317396!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMjPCsDAyJzM3LjUiTiAxMTLCsDMxJzI3LjEiRQ!5e0!3m2!1sfr!2stw!4v1448518944612" width="100%" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
-        </div>
+        <h3><?php print $field_title_address_china[0]['value'] ?></h3>
+        <?php print $field_address_china[0]['value'] ?>
+        <div class="col-xs-12 margin-top-20 padding-0"><?php print $field_map_localisation_china[0]['value'] ?></div>
     </div>
 </div>
