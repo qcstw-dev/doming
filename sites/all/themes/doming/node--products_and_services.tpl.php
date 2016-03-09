dac4d73f21f81a216626e6d711940628a9c46e86
QCS18 <QCS18@PC-18.qcs.taiwan>
2016-03-09 15:08:01 +0800

    correction links page feature and services

--- sites/all/themes/doming/node--products_and_services.tpl.php
+++ sites/all/themes/doming/node--products_and_services.tpl.php
@@ -8,7 +8,7 @@
         $blocks = entity_load('field_collection_item', $list_item_ids);
         $i = 1;
         foreach (array_slice($blocks, 0, 3) as $block) { ?>
-            <a id="a-block" href="<?php print url("node/".$block->field_block_link['und'][0]['url']) ?>" title="<?php print $block->field_block_link['und'][0]['title'] ?>">
+            <a id="a-block" href="<?php print url($block->field_block_link['und'][0]['url']) ?>" title="<?php print $block->field_block_link['und'][0]['title'] ?>">
                 <div class="col-xs-12 col-md-3 thumbnail border">
                     <img src="<?php print file_create_url($block->field_block_image['und'][0]['uri']); ?>" alt="<?php print $block->field_block_image['und'][0]['alt']; ?>" title="<?php print $block->field_block_image['und'][0]['title']; ?>" />
                     <h3 id="block-title" class="col-lg-12 text-center"><?php print $block->field_block_title['und'][0]['value'] ?></h3>
@@ -23,7 +23,7 @@
     </div>
     <div class="clearfix"></div>
     <h2 class="margin-top-0"><?php print $field_main_title_2[0]['value'] ?></h2>
-    <a id="a-block" href="<?php print url("node/".$field_block_4_5_6_link[0]['url']) ?>" title="<?php print $field_block_4_5_6_link[0]['title'] ?>">
+    <a id="a-block" href="<?php print url($field_block_4_5_6_link[0]['url']) ?>" title="<?php print $field_block_4_5_6_link[0]['title'] ?>">
         <div class="col-xs-12"><?php
             $i = 1;
             foreach (array_slice($blocks, 3, 3) as $block) { ?>
